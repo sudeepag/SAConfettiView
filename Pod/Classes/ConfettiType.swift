12017311fcbc70ebe0490c65f38a41d25a8801c5
Kyle McAlpine <k@kylejm.io>
2016-05-18 11:58:25 +0100

    Correcting image getter on ConfettiType

--- Pod/Classes/ConfettiType.swift
+++ Pod/Classes/ConfettiType.swift
@@ -16,29 +16,21 @@ public enum ConfettiType {
     case Image(UIImage)
     
     var image: UIImage? {
-        var fileName: String!
+        var imageName: String!
         
         switch self {
         case .Confetti:
-            fileName = "confetti"
+            imageName = "confetti"
         case .Triangle:
-            fileName = "triangle"
+            imageName = "triangle"
         case .Star:
-            fileName = "star"
+            imageName = "star"
         case .Diamond:
-            fileName = "diamond"
+            imageName = "diamond"
         case let .Image(customImage):
             return customImage
         }
         
-        let path = NSBundle(forClass: SAConfettiView.self).pathForResource("SAConfettiView", ofType: "bundle")
-        let bundle = NSBundle(path: path!)
-        let imagePath = bundle?.pathForResource(fileName, ofType: "png")
-        let url = NSURL(fileURLWithPath: imagePath!)
-        let data = NSData(contentsOfURL: url)
-        if let data = data {
-            return UIImage(data: data)!
-        }
-        return nil
+        return UIImage(named: imageName)
     }
 }
